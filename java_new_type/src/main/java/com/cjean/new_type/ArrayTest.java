28dc8d612c0fc59d3b8f17dbaf3df560993435ee
chu_dliang <1417261875@qq.com>
2020-03-21 14:16:08 +0800

    测试提交

--- java_new_type/src/main/java/com/cjean/new_type/ArrayTest.java
+++ java_new_type/src/main/java/com/cjean/new_type/ArrayTest.java
@@ -1,12 +1,3 @@
-/**
- * Copyright (C), 2015-2020, XXX有限公司
- * FileName: ArrayTest
- * Author:   14172
- * Date:     2020/3/21 9:11
- * History:
- * <author>          <time>          <version>          <desc>
- * 作者姓名           修改时间           版本号              描述
- */
 package com.cjean.new_type;
 
 /**
@@ -19,6 +10,7 @@ public class ArrayTest {
         long l = 9223372036854774807L;
 
         String s = ""+l;
+        System.out.println("w这是测试提交的");
         System.out.println(s);
 
     }
